c8b69a88ff13b7f317f3badfe980933ba5e18ed1
cicaf <womarow@gmail.com>
2025-03-27 17:28:37 +0300

    KKK

--- submission/03.sh
+++ submission/03.sh
@@ -4,15 +4,13 @@
 
 segwit_address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
-echo "$segwit_address"
 
-bitcoin-cli -rpcwallet=btrustwallet generatetoaddress 101 "$segwit_address"
+
+bitcoin-cli -regtest  generatetoaddress 101 "$segwit_address"
 
 
 echo $segwit_address
 
-
-#segwit_address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
 #bitcoin-cli -rpcwallet=btrustwallet generatetoaddress 101 "$segwit_address"
 
