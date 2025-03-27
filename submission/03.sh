72b1f528ca5fabd542bc1ca541855a09dd2d78ef
cicaf <womarow@gmail.com>
2025-03-27 15:39:51 +0300

    Funding Segwit Wallet

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,11 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+segwit_address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+echo $segwit_address
+
+bitcoin-cli -regtest generatetoaddress 101 "$segwit_address"
+
+echo $segwit_address
+
